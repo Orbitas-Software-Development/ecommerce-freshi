cd201fa034b737a333535f7071c49d016d4bf28c
KrauseKelsen <kkelsen09@gmail.com>
2025-01-15 22:31:53 -0600

    adding dashboardcontroller and totals

---
+++ be/ecommerce-freshydeli/Controllers/DashboardController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerce_freshydeli.Controllers
+{
+    [ApiController]
+    [Route("api/Dashboard")]
+    public class DashboardController : ControllerBase
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public DashboardController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet("getDashboardTotals")]
+        public async Task<IActionResult> GetDashboardTotals(
+            int departmentId,
+            int branchId,
+            int workflowId,
+            int companyId,
+            int userId,
+            string timeZoneOffset = "-06:00") // Valor predeterminado si no se pasa desde el frontend
+        {
+            try
+            {
+                var result = await Context.DashboardTotals
+                    .FromSqlInterpolated($@"
+                    EXEC GetDashboardTotals 
+                    @DeparmentId = {departmentId}, 
+                    @BranchId = {branchId}, 
+                    @WorkflowId = {workflowId}, 
+                    @CompanyId = {companyId}, 
+                    @UserId = {userId}
+            ")
+                    .AsNoTracking()
+                    .ToListAsync();
+
+                // Dado que el SP devuelve solo una fila, tomamos el primer elemento
+                return Ok(result.FirstOrDefault());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ be/ecommerce-freshydeli/Entities/DashboardTotals.cs
@@ -0,0 +1,13 @@
+﻿namespace ecommerce_freshydeli.Entities
+{
+    public class DashboardTotals
+    {
+        public int TotalAssets { get; set; }
+        public int TotalRequestsToday { get; set; }
+        public int TotalRequestsForMonth { get; set; }
+        public int TotalRequests { get; set; }
+        public int TotalProducts { get; set; }
+        public int TotalOrders { get; set; }
+        public int TotalPending { get; set; }
+    }
+}
